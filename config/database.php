49a5b75a2b01bc2d129c75a3be43f9f7c13df81f
Luan Carlos <luan_c_schmitz@outlook.com>
2017-12-14 14:11:18 -0200

    database heroku

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_1171c4d3216cd43'),
+            'username' => env('DB_USERNAME', 'ba92ab71fd93f3'),
+            'password' => env('DB_PASSWORD', '40146f08'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
