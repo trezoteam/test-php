3db27788802aa6540fee21dbe5a9a900d4570701
Luan Carlos <luan_c_schmitz@outlook.com>
2017-12-14 14:29:36 -0200

    force https v1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,10 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
     	Schema::defaultStringLength(191);
-    }
 
-    if($this->app->environment() == 'production') {
-	URL::forceScheme('https');
+        if($this->app->environment() == 'production') {
+        	URL::forceScheme('https');
+        }
     }
 
     /**
