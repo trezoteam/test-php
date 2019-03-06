860bdcde44d819bd90da5d57695f77063258ea89
Walisson Ferreira <walisson.ferreira@gmail.com>
2019-03-06 08:22:42 -0300

    Product dimension attribute auto map added

--- Walisson/TrezoShippingTest/Helper/Shipping/Quote/Adapter.php
+++ Walisson/TrezoShippingTest/Helper/Shipping/Quote/Adapter.php
@@ -17,6 +17,10 @@ use Walisson\TrezoShippingTest\Helper\Config;
  */
 class Adapter extends \Magento\Framework\App\Helper\AbstractHelper
 {
+    const MIN_WEIGHT = 0.3;
+    const MIN_LENGTH = 10;
+    const MIN_HEIGHT = 10;
+    const MIN_WIDTH = 10;
 
     /** @var string */
     const SHIPPING_QUOTE_ENDPOINT = 'shipping/quote';
@@ -91,18 +95,44 @@ class Adapter extends \Magento\Framework\App\Helper\AbstractHelper
                 continue;
             }
 
+            $product = $item->getProduct();
+            /**
+             * Here we can use many strategies, as create the dimensional attributes, map already created attributes and so on.
+             * For the test I've used just a "auto map" to find some attribute that has the dimension name on it's name, and it's not a real world ideal implementation,
+             * and in this case, maybe a attribute map on the method admin configuration with creation of an EAV in case it doesn't exist.
+             */
             $apiItems[] =
                 [
-                    'Weight' => $item->getWeight() ?? 0.3,
-                    'Length' => $item->getLength() ?? 10,
-                    'Height' => $item->getHeight() ?? 10,
-                    'Width' => $item->getWidth() ?? 10,
+                    'Weight' => $product->getWeight() ?? $this->getProductDimension($product, 'WEIGHT'),
+                    'Length' => $product->getLength() ?? $this->getProductDimension($product, 'LENGTH'),
+                    'Height' => $product->getHeight() ?? $this->getProductDimension($product, 'HEIGHT'),
+                    'Width' => $product->getWidth() ?? $this->getProductDimension($product, 'WIDTH'),
                     'SKU' => (string)$item->getSku(),
                     'Quantity' => $item->getQty()
                 ];
         }
 
         return $apiItems;
+    }
+
+    /**
+     * Get a product attribute value that has the requested attribute name on it.
+     *
+     * @param $product
+     * @param $attribute_name
+     * @return float|string
+     */
+    private function getProductDimension($product, $attribute_name)
+    {
+        $attributes = $product->getAttributes();
+
+        foreach ($attributes as $attribute) {
+            if (strpos($attribute->getAttributeCode(), strtolower($attribute_name)) !== false) {
+                return (float)$product->getData($attribute->getAttributeCode());
+            }
+        }
+
+        return self::MIN_ . $attribute_name;
     }
 
 }
